de17d80ab5532b01657ce6b7b04222ae943c4c0e
Hongarc <anhhong12121997t@gmail.com>
2017-03-10 13:12:40 +0700

    Upload new code

---
+++ js/popup.js
@@ -0,0 +1,44 @@
+count_like();
+$(document).ready(function(){
+	$("#like-btn").click(like);
+	$("#unlike-btn").click(unlike);
+});
+
+function count_like() {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {fbLike: "getLike"}, function(response) {
+			$("#number_like").html(response.like);
+			$("#number_unlike").html(response.unlike);
+		});
+	});
+}
+
+
+function like() {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {fbLike: "like"}, function(response) {
+			$("#like-btn").html("Đã like tất cả " + response.number + "  mục");
+			$("#like-btn").fadeOut(2000);
+			setTimeout(function(){
+				$("#like-btn").html("Like All");
+			}, 2000);
+			$("#like-btn").fadeIn(100);
+			count_like();
+		});
+	});
+}
+
+
+function unlike() {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {fbLike: "unlike"}, function(response) {
+			$("#unlike-btn").html("Đã unlike tất cả " + response.number + "  mục");
+			$("#unlike-btn").fadeOut(2000);
+			setTimeout(function(){
+				$("#unlike-btn").html("UnLike All");
+			},2000);
+			$("#unlike-btn").fadeIn(100);
+			count_like();
+		});
+	});
+}
