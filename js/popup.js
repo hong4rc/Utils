61e1c39694257f86ec4a59abec9bc22d7d32a273
Hongarc <anhhong12121997t@gmail.com>
2017-03-10 13:21:01 +0700

    Add css to my code

--- js/popup.js
+++ js/popup.js
@@ -17,10 +17,10 @@ function count_like() {
 function like() {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {fbLike: "like"}, function(response) {
-			$("#like-btn").html("Đã like tất cả " + response.number + "  mục");
+			$("#like-btn span").html("Đã like tất cả " + response.number + "  mục");
 			$("#like-btn").fadeOut(2000);
 			setTimeout(function(){
-				$("#like-btn").html("Like All");
+				$("#like-btn span").html("Like All");
 			}, 2000);
 			$("#like-btn").fadeIn(100);
 			count_like();
@@ -32,10 +32,10 @@ function like() {
 function unlike() {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {fbLike: "unlike"}, function(response) {
-			$("#unlike-btn").html("Đã unlike tất cả " + response.number + "  mục");
+			$("#unlike-btn span").html("Đã unlike tất cả " + response.number + "  mục");
 			$("#unlike-btn").fadeOut(2000);
 			setTimeout(function(){
-				$("#unlike-btn").html("UnLike All");
+				$("#unlike-btn span").html("UnLike All");
 			},2000);
 			$("#unlike-btn").fadeIn(100);
 			count_like();
