450744eb7ad958ae54f388b28f339724b4303f29
Hongarc <anhhong12121997t@gmail.com>
2017-03-11 10:40:53 +0700

    Add tab to anythings

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,24 @@
 count_like();
 $(document).ready(function(){
+	tab();
 	$("#like-btn").click(like);
 	$("#unlike-btn").click(unlike);
 });
 
+function tab (){
+	jQuery('.tabs .tab-links a').on('click', function(e)  {
+		var currentAttrValue = jQuery(this).attr('href');
+ 
+        // Show/Hide Tabs
+        // jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+
+        jQuery('.tabs ' + currentAttrValue).slideDown(400).siblings().slideUp(400);
+        // Change/remove current tab to active
+        jQuery(this).parent('.tab-links').addClass('active').siblings().removeClass('active');
+ console.log(this);
+        e.preventDefault();
+    });
+}
 function count_like() {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {fbLike: "getLike"}, function(response) {
