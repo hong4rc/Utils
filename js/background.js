bdf219267ac1a5b7615190750ea1e0d57451c037
Hongarc <anhhong12121997t@gmail.com>
2017-04-27 13:37:41 +0700

    Change Oxford to Vdict

--- js/background.js
+++ js/background.js
@@ -8,14 +8,15 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 var menuItem = {
     id: "anhhong",
-    title: "Translate with oxford",
+    title: "Translate with vdict",
     contexts: ["selection"]
 }
 chrome.contextMenus.create(menuItem);
 chrome.contextMenus.onClicked.addListener(function(data) {
     if (data.menuItemId === "anhhong" && data.selectionText) {
-        var link = 'http://www.oxfordlearnersdictionaries.com/definition/english/get-out_2?q=';
-        link += encodeURI(data.selectionText);
+        var link = 'https://vdict.com/';
+        var dt = encodeURI(data.selectionText);
+        link += dt + ',1,0,0.html';
         var createData = {
             url: link,
             type: "popup",
